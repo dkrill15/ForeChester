dd06bdfcbe969b6a091dd4675814d07ed3dd8679
dkrill15 <dkrill@nd.edu>
2024-03-04 10:02:59 -0500

    clean forechester

--- forechester.py
+++ forechester.py
@@ -1,14 +1,10 @@
-from flask import Flask, make_response, jsonify, request
+from flask import Flask, make_response
 from flask_cors import CORS
 import xml.etree.ElementTree as ET
-import csv
-from bs4 import BeautifulSoup
 import html_parser
 import os
 import json
 from functions import *
-from round_class import round_class, Product
-
 
 app = Flask(__name__)
 CORS(app)
@@ -30,10 +26,6 @@ def make_xml_response(avgs, wscores):
 
 @app.route('/score')
 def score(prc=0, seg=0, rvm=0, mto=0, mtn=0, ago=0, szo=0, szn=0, pfo=0, pfn=0, prm=0, awa=0, sal=0, acc=0, arp=45, test=0, rd=0, up=1, new=0, debug=0):
-
-    #new commnet on new branch
-
-    
     # get preferences for each segment
     file_path = 'couriers/json_prefs/round' + str(rd) + 'prefs.json'
     if os.path.exists(file_path):
@@ -116,108 +108,6 @@ def score(prc=0, seg=0, rvm=0, mto=0, mtn=0, ago=0, szo=0, szn=0, pfo=0, pfn=0, 
     # if debug:
     #     print(wscores)
     return averages
-
-
-    # return xml object for Google Sheet
-    data = ET.Element("data")
-
-    # if int(inputs.get('brk', 0)):
-    #     for sublist in [price_score, mtbf_score, age_score, position_score]:
-    #         item = ET.SubElement(data, 'Item')
-    #         for i, value in enumerate(sublist):
-    #             # Creating sub-elements for each item in the sublist
-    #             subelement = ET.SubElement(item, f'Field{i+1}')
-    #             subelement.text = str(value)
-    #     xml_data = ET.tostring(data, encoding='unicode')
-    #     response = make_response(xml_data)
-    #     response.headers['Content-Type'] = 'application/xml'
-    #     return response
-
-
-    for avg in averages:
-        item = ET.SubElement(data, "item")
-        item.text = str(avg)
-    for w in wscores:
-        item = ET.SubElement(data, "item")
-        item.text = str(w)
-
-    xml_data = ET.tostring(data, encoding='unicode')
-
-    response = make_response(xml_data)
-    response.headers['Content-Type'] = 'application/xml'
-    return response
-
-
-# @app.route('/post_product')
-# def post_product():
-#     inputs = request.args
-#     # if float(inputs['run']) > 0:
-#     #     root = ET.Element('root')
-#     #     return ET.tostring(root)
-#     prc = float(inputs['prc'])
-#     rvm = int(inputs['rvm'])
-#     mto = int(inputs['mto'])
-#     mtn = int(inputs['mtn'])
-#     ago = float(inputs['ago'])
-#     szo = float(inputs['szo'])
-#     szn = float(inputs['szn'])
-#     pfo = float(inputs['pfo'])
-#     pfn = float(inputs['pfn'])
-#     prm = float(inputs['prm'])
-#     awa = float(inputs['awa'])
-#     sal = float(inputs['sal'])
-#     arp = float(inputs['arp'])
-#     rd = int(inputs['rd'])
-#     up = int(inputs.get('up', 1))
-#     new = int(inputs.get('new', 0))
-#     product_name = inputs['name']
-
-#     rounds[rd+1].products[product_name] = Product(product_name, prc, rvm, mto, mtn, ago, szo, szn, pfo, pfn, prm, awa, sal, arp, rd, up, new)
-
-#     for p in rounds[rd+1].products:
-#         print(rounds[rd+1].products[p])
-
-#     root = ET.Element('root')
-#     item = ET.SubElement(root, 'item')
-#     item.text = str(int(1))
-#     return ET.tostring(root)
-
-
-# @app.route('/match_product')
-# def match_product():
-#     inputs = request.args
-#     prc = float(inputs['prc'])
-#     rvm = float(inputs['rvm'])
-#     mto = int(inputs['mto'])
-#     mtn = int(inputs['mtn'])
-#     ago = float(inputs['ago'])
-#     szo = float(inputs['szo'])
-#     szn = float(inputs['szn'])
-#     pfo = float(inputs['pfo'])
-#     pfn = float(inputs['pfn'])
-#     prm = float(inputs['prm'])
-#     awa = float(inputs['awa'])
-#     sal = float(inputs['sal'])
-#     arp = float(inputs['arp'])
-#     rd = int(inputs['rd'])
-#     up = int(inputs.get('up', 1))
-#     new = int(inputs.get('new', 0))
-#     product_name = inputs['name']
-
-#     prod = rounds[rd+1].products.get(product_name, None)
-#     change_detected = 1
-#     if prod is not None and Product(product_name, prc, rvm, mto, mtn, ago, szo, szn, pfo, pfn, prm, awa, sal, arp, rd, up, new) == prod:
-#         change_detected = 0
-        
-#     #return xml object that is 1 if change_detected0 if not
-#     root = ET.Element('root')
-#     item = ET.SubElement(root, 'item')
-#     item.text = str(int(change_detected))
-#     return ET.tostring(root)
-
-
-# rounds = [round_class(i) for i in range(1, 9)]
-
 
 if __name__ == '__main__':
     app.run(debug=True, port=5040)
